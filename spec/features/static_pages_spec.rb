c2e73f907b14c0aa50ae727f680405db8fcfb64c
项阳阳 <xyy@weavi.com>
2015-04-08 16:21:13 +0800

    Finish static pages

---
+++ spec/features/static_pages_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "Static pages" do
+  describe "Home page" do
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_selector('h1', :text => 'Sample App')
+    end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample APP | Home')
+    end
+  end
+
+  describe "Help page" do
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_selector('h1', :text => 'Help')
+    end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample APP | Help')
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_selector( 'h1', :text => 'About Us')
+    end
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title( 'Ruby on Rails Tutorial Sample APP | About Us')
+    end
+  end
+end
+
