9bd3e14d716750f5a6b8146230d1523bb5dceee2
项阳阳 <xyy@weavi.com>
2015-04-09 13:45:17 +0800

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'pg','~>0.15.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -34,9 +34,14 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
   gem 'rspec-rails'
+  gem 'guard-rspec'
+  gem 'guard-spork'
+  gem 'spork'
 end
 
 group :test do
   gem 'capybara', '~> 2.4.4'
+  gem 'rb-fsevent', :require => false
+  gem 'growl'
   end
 
--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Static pages" do
+  let(:base_title) {"Ruby on Rails Tutorial Sample APP"}
   describe "Home page" do
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
@@ -8,8 +9,9 @@ RSpec.describe "Static pages" do
     end
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample APP | Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
+
   end
 
   describe "Help page" do
@@ -19,7 +21,7 @@ RSpec.describe "Static pages" do
     end
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample APP | Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -31,7 +33,19 @@ RSpec.describe "Static pages" do
     end
     it "should have the h1 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title( 'Ruby on Rails Tutorial Sample APP | About Us')
+      expect(page).to have_title( "#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector( 'h1', :text => 'Contact Us')
+    end
+    it "should have the h1 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title( "#{base_title} | Contact")
     end
   end
 end
